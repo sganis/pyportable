8ce5f2a8b9440ae98fc428429eccc7632a6bba7a
sganis <sganis@gmail.com>
2015-05-16 03:35:00 +0300

    First commit

---
+++ README.rst
@@ -0,0 +1,36 @@
+Portable Python
+===============
+
+Portable Python is a the python interpreter (http://python.org) that can run from a folder, any USB storage device, local or network location so you can enjoy a powerful and portable programming environment. 
+
+This is a fork of the excellent work from https://github.com/pericazivkovic/portablepython. The motivations to fork the original project were the need to have an updated portable version of python 2.7 in Windows, 64-bit packages, maximize the use of pip, and also make the build script simpler. I tried to simplify the original project as much as possible. There is only one script, and shorter. The steps of creating a portable python are as follows: 
+
+- Download and extract python.
+- set the new PATH environment variable so you python is the new executable just donwloaded.
+- Download pip and install it. This became quite simple with the git-pip.py script. Setuptools will be installed too.
+- Download numpy and scipy, I am using the latest compilations from http://www.lfd.uci.edu/~gohlke/pythonlibs. I decided to use these precompiled packages because they require some extra libraries such as lapack, blas and others, and didn't want to setup such building environment. In addition, these packages will install almost all dependencies needed for any other package.
+- Use pip to install the rest of packages.
+- It is aslo possible to use pip to install packages that require compilation, such as cython. The trick is to install Visual Studio C++ (I have version 2012), and set this environment variable, so pip will find the right compiler:
+
+.. code:: bash
+
+	c:\portablepython> SET VS90COMNTOOLS=%VS110COMNTOOLS%
+
+In my machine, VS110COMNTOOLS is pointing to `C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\`, but I guess any version of Visual Studio should work.
+
+
+Usage
+-----
+
+The build.bat script will make a portable version of python. It downloads packages from internet, and creates a self-extracting executable. Run the script from a windows terminal:
+
+.. code:: bash
+	
+	c:\portablepython> build.bat
+
+
+Licensing
+---------
+Portable Python build scripts are licensed under MIT license, other third party tools and packages are licensed by their respecitive projects/owners.
+
+
---
+++ build.bat
@@ -0,0 +1,85 @@
+:: build script to create a portable python for windows
+:: version 	2.7.10rc1
+:: author: 	sganis
+:: date: 	05/16/2015
+
+@echo off
+
+:: update these values :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set PYTHON_URL=https://www.python.org/ftp/python/2.7.10/python-2.7.10rc1.amd64.msi
+set PIP_URL=https://bootstrap.pypa.io/get-pip.py
+set NUMPY_URL=http://www.lfd.uci.edu/~gohlke/pythonlibs/r7to5k3j/numpy-1.9.2+mkl-cp27-none-win_amd64.whl
+set SCIPY_URL=http://www.lfd.uci.edu/~gohlke/pythonlibs/r7to5k3j/scipy-0.15.1-cp27-none-win_amd64.whl
+
+:: this list of packages will be install using pip
+::set PACKAGES=dateutils,ipython,pyreadline,sphinx,pillow,matplotlib,pandas
+set PACKAGES=
+:: no more update needed from here :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+echo Portable Python build script started... 
+
+:: prepare folders
+:: script directory without trailing backslash 
+set DIR=%~dp0
+IF %DIR:~-1%==\ SET DIR=%DIR:~0,-1%
+set PORTABLE=%DIR%\PortablePython
+set DOWNLOADS=%DIR%\Downloads
+if not exist %PORTABLE%  mkdir %PORTABLE%
+if not exist %DOWNLOADS% mkdir %DOWNLOADS%
+	
+:: python
+call :download %PYTHON_URL%
+echo filename: %filename%
+echo Extracting python...
+msiexec /quiet /a %DOWNLOADS%\%filename% TARGETDIR="%PORTABLE%"
+
+:: new path
+set PATH=%PORTABLE%;%PORTABLE%\Scripts;%PATH%
+
+:: pip
+call :download %PIP_URL%
+python "%DOWNLOADS%\get-pip.py"
+
+:: numpy
+call :download %NUMPY_URL%
+pip install "%DOWNLOADS%\%filename%"
+
+:: scipy
+call :download %SCIPY_URL%
+pip install "%DOWNLOADS%\%filename%"
+
+:: install list of packages
+for %%i in (%PACKAGES%) do (
+  pip install %%i
+)
+
+:: test
+python %DIR%\test.py
+
+::  installer
+echo creating compressed package...
+%DIR%\tools\7za.exe a PortablePython.exe -mmt -mx9 -sfx7z.sfx PortablePython >NUL
+
+:: end
+goto :eof
+
+
+:: functions
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:download url
+:: download file from url
+:: extract filename from url and return filename
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SETLOCAL
+FOR /f %%i IN ("%1") DO set filename=%%~nxi
+set file=%DOWNLOADS%\%filename%
+echo filename: %filename%
+IF NOT EXIST %file% (
+	echo downlowing %1...
+	tools\wget.exe -U Mozilla/5.0 --progress=bar --no-check-certificate -N -P "%DOWNLOADS%" %1 ::2>NUL
+) else (
+	echo %file% exists, using cache version...
+)
+ENDLOCAL & SET filename=%filename%
+goto :eof
+::::::::::::::::::::::::::::::::::::::::::::
